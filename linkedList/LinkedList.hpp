285ecccc63c113a61db4b9aecf402df755211c4b
Sean Cunningham <47498393+SeanCBlue652@users.noreply.github.com>
2020-09-06 18:51:39 -0500

    Updated the size() and search() methods

--- linkedList/LinkedList.hpp
+++ linkedList/LinkedList.hpp
@@ -29,10 +29,7 @@ bool LinkedList<T>::isEmpty() const
 template <typename T>
 int LinkedList<T>::size() const
 {
-	/** TODO 
-		Fix this method
-	*/
-	return(0);
+	return(m_size);
 }
 
 template <typename T>
@@ -41,9 +38,18 @@ bool LinkedList<T>::search(T value) const
 	Node<T>* temp = m_front;
 	bool isFound = false;
 
-	/** TODO 
-		Fix this method
-	*/
+	do
+	{
+			if (temp->getValue == value) {
+				isFound = true;
+				break;
+			}
+			else {
+				temp = temp->getNext();
+			}
+		
+	} while (!(temp == nullptr));
+	
 
 	return(isFound);
 }
